980261947b5c90893ec72992bc40d2462d15cd30
unknown <smcnicol@gmail.com>
2014-07-08 23:05:12 +1000

    Euler177

--- problems/Euler177.py
+++ problems/Euler177.py
@@ -2,34 +2,43 @@ from math import sin,cos,radians,atan2,degrees
 
 
 
+C = [cos(radians(t)) for t in range(360)]
+S = [sin(radians(t)) for t in range(360)]
 
-# counter = 0
-# for w in range(1,180):
-# 	for x in range(1,180-w):
-# 		for y in range(1,180-w):
-# 			for z in range(1,min(180-x,180-y)):
-# 				if w != x:
-# 					a,b = abs(sin(radians(x))/sin(radians(w-x))),abs(sin(radians(w))/sin(radians(w-x)))
-# 					print a*sin(cos(w)) - b*sin(cos(x))
-# 				counter +=1
-# 	print w,counter
+def f(w,x,y,z):
+	a,b = S[x]/S[w+x], S[w]/S[w+x]
+	c,d = S[z]/S[y+z], S[y]/S[y+z]
+	s = degrees(atan2(a*S[w+y],c-a*C[w+y]))
+	return s,abs(round(s)-s)<1e-9
 
 
-
-counter = 0
+counter =01
 for w in range(1,180):
 	for x in range(1,180-w):
-		if x != w:
-			a,b = (sin(radians(x))/sin(radians(w+x))),(sin(radians(w))/sin(radians(w+x)))
-			for y in range(1,180-w):
-				for z in range(1,min(180-y,180-x)):
-					c,d = (sin(radians(z))/sin(radians(y+z))),(sin(radians(y))/sin(radians(y+z)))
-					try:
-						s = degrees(atan2(a*sin(radians(w+y)),(c-a*cos(radians(w+y)))))
-						if abs(round(s)-s)<1e-9:
-							counter +=1
-					except:
-						raise
+		for y in range(1,180-w):
+			maxz = min(180-y,180-x)
+			for z in range(1,maxz):
+				s,sol = f(w,x,y,z)
+				if sol:
+					counter +=1
 
-						#print counter,w,x,y,z,round(s)
 	print w,counter
+print counter
+
+#counter = 01
+#for w in range(1,180):
+	# for x in range(1,180-w):
+	# 	if x != w:
+	# 		a,b = (sin(radians(x))/sin(radians(w+x))),(sin(radians(w))/sin(radians(w+x)))
+	# 		for y in range(1,180-w):
+	# 			for z in range(1,min(180-y,180-x)):
+	# 				if w <= y and w+y <= 90:
+	# 					c,d = (sin(radians(z))/sin(radians(y+z))),(sin(radians(y))/sin(radians(y+z)))
+	# 					s = degrees(atan2(a*sin(radians(w+y)),(c-a*cos(radians(w+y)))))
+	# 					if abs(round(s)-s)<1e-9:
+	# 						print w,x,y,z
+	# 						counter +=1
+	# 					else:
+	# 						print w,x,y,z
+
+	# print w,counter
