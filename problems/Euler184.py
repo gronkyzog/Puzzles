a6d23e419bd8290fe8b1e8d73fd509c438907a9f
unknown <smcnicol@gmail.com>
2014-08-10 23:22:22 +1000

    Euler184

--- problems/Euler184.py
+++ problems/Euler184.py
@@ -1,36 +1,23 @@
 import eulertools 
+import math
 
 
-def det(a,b):
-	return a[0]*b[1]-b[0]*a[1]
 
-r = 10
+
+r = 105
 
 primitivepoints = {}
 for x in range(-r+1,r):
 	for y in range(-r+1,r):
-		if eulertools.gcd(x,y) in (-1,1) and x**2 +y**2 < r**2:
-			primitivepoints[(x,y)]=len([1 for k in range(1,r+1) if k**2*(x**2 + y**2) < r**2])
+		if  x**2 +y**2 < r**2 and (x,y)!= (0,0):
+			k = abs(eulertools.gcd(x,y))
+			key = (x/k,y/k)
+			if key not in primitivepoints:
+				primitivepoints[key]=0
+			primitivepoints[key]+=1
 
 
-total = 0
-for v1,n1 in primitivepoints.items():
-	if v1[0]>0:
-		for v2,n2 in primitivepoints.items():
-			if det(v1,v2)>0:
-				for v3,n3 in primitivepoints.items():
-					if v3[0]<0:
-						if det(v1,v2) >0 and det(v2,v3)>0 and det(v3,v1)>0 and v1>v2>v3:
-								total += n1*n2*n3
-		print v1,total
-
-print total*2
-
-
-#v1[0]*v2[1] > v2[0]*v1[1]
-#v2[0]*v3[1] > v3[0]*v2[1]
-#v3[0]*v1[1] > v1[0]*v3[1]
-
-
+for v in primitivepoints.values():
+	print v
 
 
