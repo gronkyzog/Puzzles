c595763ff3e5dd78f4ba8f367c84d753a6610b95
gronkyzog <smcnicol@gmail.com>
2014-09-02 22:52:49 +1000

    Zdd

--- zdd.py
+++ zdd.py
@@ -40,12 +40,12 @@ def Symmetric(k,X,U):
 	if k==0 and len(U) == 0:
 		val = tuple([-1,1,1])
 		head = hash(val)
-		return {head:val},head
+		return {0:val,head:val},head
 
 	elif (k!=0 and len(U) == 0) or k < 0:
 		val = tuple([-1,0,0])
 		head = hash(val)
-		return {head:val},head
+		return {0:val,head:val},head
 
 	else:
 		v = min(U)
@@ -59,18 +59,36 @@ def Symmetric(k,X,U):
 			Zdd = {head:val}
 			Zdd.update(Zdd0)
 			Zdd.update(Zdd1)
+			Zdd[0] = head
 			return Zdd,head
-
 		else:
 			Zdd0,head0 = Symmetric(k,X0,U0)	
 			val = tuple([v,head0,head0])
 			head = hash(val)
 			Zdd = {head:val}
 			Zdd.update(Zdd0)
+			Zdd[0] = head
 			return Zdd,head
 
 
 
+def mergeAnd(F,G):
+	Zdd = {}
+	v,_,_ = F[0]
+	w = G[0]
+	if v==w:
+		headF = F[0]
+		headG = F[0]
+
+		F0 = {k:v for k,v in F.items() if k[0]!=v}
+		G0 = {k:v for k,v in G.items() if k[0]!=v}
+		val = tuple([v,head0,head0])
+		head = hash(val)
+		Zdd = {head:val}
+		Zdd.update(F0)
+		Zdd.update(G0)
+		Zdd[0] = head
+		return Zdd,head
 
 
 
@@ -79,28 +97,36 @@ def Symmetric(k,X,U):
 
 
 
-def cycle(row):
+
+
+
+
+def cycle(ZDD,row):
 	node,low,hi = ZDD[row]
 	if node ==-1:
 		if low==1:
 			yield []
 
 	else:
-		for x in cycle(low):
+		for x in cycle(ZDD,low):
 			# the current node is not included
 			yield x
-		for x in cycle(hi):
+		for x in cycle(ZDD,hi):
 			# the current node is included
 			temp = [node]
 			temp.extend(x)
 			yield temp
 
 
-ZDD,head = Symmetric(3,range(16),range(160))
-ZDD[0] = ZDD[head] 
+ZDD1,head1 = Symmetric(3,[1,2,3,4,5,6,7],[1,2,3,4,5,6,7,8,9,10])
+ZDD1[0] = ZDD1[head1] 
 
-print len(ZDD)
+ZDD2,head2 = Symmetric(2,[1,2,8,9,10],[1,2,3,4,5,6,7,8,9,10])
+ZDD2[0] = ZDD2[head2] 
 
+
+for x in cycle(ZDD1,0):
+	print x
 
 
 
