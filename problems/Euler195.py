aa6c993952322ee3fae4d642d091f8a39e603119
unknown <smcnicol@gmail.com>
2014-09-29 23:45:52 +1000

    Euler195

--- problems/Euler195.py
+++ problems/Euler195.py
@@ -19,7 +19,7 @@ def generate_primative_60(rmax):
 				#a = m**2 - m*n + n**2
 				#b = 2*m*n - n**2
 				#c = m**2 - n**2
-				# a**2 = b**2 + c**2 - b*c
+				#b**2 == a**2 + c**2 - a*c
 
 				k = eulertools.gcd(a,eulertools.gcd(b,c))
 				a0,b0,c0 = a/k,b/k,c/k
@@ -29,10 +29,11 @@ def generate_primative_60(rmax):
 				for s in range(1,200):
 					a,b,c = s*a0,s*b0,s*c0
 					r = calc_radius(a,b,c)
+					print r, ((1.*a*c)/(a+b+c))*(3**0.5)/2
 					if r > 100:
 						break
 					else:	
-						yield (m,n,s), [a,b,c,b**2==a**2 + c**2 - a*c]
+						yield (m,n,s), [a,b,c,k,s]
 output = set()
 for i,(z,x) in enumerate(generate_primative_60(100),start=1):
 	y = sorted(x)
