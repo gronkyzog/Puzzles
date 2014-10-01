25e688fd4617ac77f485ef96057cd82a515c1f2d
unknown <smcnicol@gmail.com>
2014-10-01 23:37:31 +1000

    Euler195;

--- problems/Euler195.py
+++ problems/Euler195.py
@@ -1,4 +1,4 @@
-import eulertools
+from  eulertools import gcd
 
 
 def calc_radius(a,b,c):
@@ -8,34 +8,56 @@ def calc_radius(a,b,c):
 
 
 def generate_primative_60(rmax):
-	for m in range(1,500):
+	for m in range(1,1000):
 		for n in range(1,m):
-			if eulertools.gcd(m,n) >1:
+			if gcd(m,n) > 1:
+				continue
+			a0 = m**2 - n**2
+			b0 = 2*m*n - n**2
+			c0 = m**2 - m*n + n**2
+			if a0==b0:
+				continue
+			k = gcd(a0,gcd(b0,c0))
+			a0,b0,c0 = a0/k,b0/k,c0/k
+			for s in range(1,1000):
+				a,b,c = s*a0,s*b0,s*c0
+				r = s*(3**0.5/(2*k))*n*(m-n)
+				if r<= 100:
+					yield (a,b,c),(m,n,k,s)
+				else:
+					break
+
+
+
+def generate_count(rmax):
+	counter = 0
+	mmax = int(rmax*8/(3**0.5))
+	for m in range(1,mmax):
+		for n in range(1,m/2+1):
+			if gcd(m,n) > 1:
+				continue
+			a = m**2 - n**2
+			b = 2*m*n - n**2
+			c = m**2 - m*n + n**2
+			if a==b:
 				continue
 			else:
-				a = 4*m*n
-				b = 3*m**2 + n**2
-				c = 2*m*n + abs(3*m**2 - n**2)
-				#a = m**2 - m*n + n**2
-				#b = 2*m*n - n**2
-				#c = m**2 - n**2
-				#b**2 == a**2 + c**2 - a*c
+				k = gcd(a,gcd(b,c))
+				r = (3**0.5/(2*k))*n*(m-n)
+				if r<= rmax:
+						bump = int(rmax/r)
+						print m,n,k,bump
+						counter += bump
+				
+				#else:
+				#	break
 
-				k = eulertools.gcd(a,eulertools.gcd(b,c))
-				a0,b0,c0 = a/k,b/k,c/k
-				if a0==b0==c0:
-					continue
+	return counter
+				
 
-				for s in range(1,200):
-					a,b,c = s*a0,s*b0,s*c0
-					r = calc_radius(a,b,c)
-					print r, ((1.*a*c)/(a+b+c))*(3**0.5)/2
-					if r > 100:
-						break
-					else:	
-						yield (m,n,s), [a,b,c,k,s]
-output = set()
-for i,(z,x) in enumerate(generate_primative_60(100),start=1):
-	y = sorted(x)
-	output.add(tuple(y))
-	print i,x,len(output),z
+
+
+#a*n*(m-n)
+#peak=a*(m-2*n)
+
+print generate_count(10000)
