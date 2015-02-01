b66ff3e9d506ad66babb3d95ca56b5c2553877c8
unknown <mcnicols@L28D244A73FC1.global.anz.com>
2015-02-01 20:09:01 +1100

    Solved 221

--- problems/Euler221.py
+++ problems/Euler221.py
@@ -1,53 +1,82 @@
-counter = 0
-output = set()
-n = 100000
-for p in range(1,n):
-	if p% 1000==0:
-		sx = min(len(output),150000)
-		print p,sx,len(output),sorted(list(output))[sx-1]
-	for q in range(p+1,2*p+1):
-		r,s = divmod(1+p*q,q-p)
-		if s ==0:
-			A = p*q*r
+# try to solve (p-r)*(q-r) = 1+r**2
+# factorise 1+r**2 over primes P, use x = mod_sqrt(-1,p) to get all prime factors
+# find solutions r<p<q , r < soln**(1/3)
+# assume that solution is no bigger than 150000**3 
+
+import itertools 
+import eulertools
+
+def factors(x,P):
+	if x == 1:
+		return [1]
+	r = x
+	N = [0 for p in P]
+	for i,p in enumerate(P):
+		counter = 0
+		while r % p ==0:
+			r = r/p
+			counter +=1
+		N[i]=counter
+	F = []
+	Powers=[range(n+1) for n in N]
+	for  E in itertools.product(*Powers):
+		f = eulertools.product([p**e for p,e in zip(P,E)])
+		F.append(f)
+	return F
+
+def alexandrian(n):
+	R = [r**2 + 1 for r in range(n+1)]
+	P = eulertools.primesieve(n)
+	R = [[] for r in range(n+1)]
+	for p in P:
+		soln = eulertools.modsqrt_prime(-1,p)
+		for s in soln:
+			for k in xrange(s,n+1,p):
+				r2 = k**2 +1
+				R[k].append(p)
+	
+	F = []
+	for r,V in enumerate(R):
+		r2 = r**2 +1
+		F.append(factors(r2,V))
+
+				 
+
+	output = set()
+	for r,X in enumerate(F):
+		if r==0:
+			continue
+		r2 = r**2 +1
+		for x in X:
+			p = x+r
+			q = (r2/x) + r
+			A = abs(r*p*q)
+			if p*q - p*r - q*r != 1:
+				raise
 			output.add(A)
 
-print len(output)
-print sorted(list(output))[150000-1]
-#print 196565502887779200
+	return sorted(list(output))
 
 
+def alexandrian2(n):
+	output = set()
+	for p in range(1,n):
+		for q in xrange(p+1,2*p+1):
+			r,s = divmod(1+p*q,q-p)
+			if s==0:
+				if r < q:
+					raise
+				A = p*q*r
+				if A == 342108:
+					print p,q,r
+				output.add(A)
+
+	return sorted(list(output))
 
 
+n =150000
+A =  alexandrian(n)
+print len(A)
+print A[n-1]
+print 1884161251122450
 
-# import eulertools
-
-# n = 1000000
-# R = [r**2 + 1 for r in range(n+1)]
-# P = eulertools.primesieve(n)
-# R = [[1] for r in range(n+1)]
-# for p in P:
-# 	soln = eulertools.modsqrt_prime(-1,p)
-# 	for s in soln:
-# 		for k in xrange(s,n+1,p):
-# 			r2 = k**2 +1
-# 			R[k].append(p)
-# 			R[k].append(r2/p)
-# 			R[k].append(-p)
-# 			R[k].append(-r2/p)
-
-
-# output = set()
-# for r,X in enumerate(R):
-# 	r2 = r**2 +1
-# 	for x in X:
-# 		p = x+r
-# 		if r2%x !=0:
-# 			raise
-# 		q = (r2/x) + r
-# 		A = abs(r*p*q)
-# 		if p*q - p*r - q*r != 1:
-# 			raise
-# 		output.add(A)
-
-# print sorted(list(output))[150000]
-# print 1884161251122450		
